50873e4a1289d2ec9a3b3e79f766d2da7a4af1b3
JennyJoscelyn <jennyjoscelyn10@gmail.com>
2026-08-11 19:56:51 +1000

    Prepare backend for cloud deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import db from "./db.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
